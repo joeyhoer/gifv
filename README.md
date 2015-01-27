67e10dd2a645d0e57e094071749d8c0e1d1513d0
Joey Hoer <joey@gaugeinteractive.com>
2015-01-27 12:43:19 -0500

    Adding gifv.sh and updating README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gifv
-Turn gifs and videos into gif-like videos
+
+gifv is a shell script that converts videos and GIFs into video files optimized for GIF-like playback on the web.
+
+## History
+
+Video files are superior to GIFs in filesize, playback control, and quality. There are a few online services that will convert GIFs to videos with GIF-like playback. Essentially, these "formats" are wrappers around compressed video files (h.264 encoded MP4s or WebMs), that occasionally include a GIF falback. These videos are often optimized further for GIF-like web consumption.
+
+[4chan began accepting WebMs as an alternative to GIFs in April 2014](http://blog.4chan.org/post/81896300203/webm-support-on-4chan), so long as the videos contain only one video stream, no audio streams, are shorter than 120 seconds in duration, are less than 2048x2048 in dimension, and are less than 3 MB in size.
+
+Two "competing formats", which appear to be very similar in implementation are GIFV and GFY – both pronounced "jiffy". **GIFV** was [pioneered by imgur](http://imgur.com/blog/2014/10/09/introducing-gifv/), while **GFY** was [developed by gfycat](http://www.gfycat.com/about).
+
+**DISCLAIMER:** This tool is in no way related to imgur or gfycat.
+
+## Installation
+
+Download the [`gifv` script](https://raw.githubusercontent.com/joeyhoer/gifv/master/gifv.sh) and make it available in your `PATH`.
+
+    curl -o /usr/local/bin/gifv -O https://raw.githubusercontent.com/joeyhoer/gifv/master/gifv.sh && \
+    chmod +x /usr/local/bin/gifv
+ 
+## Dependencies
+
+This script relies on `ffmpeg`:
+
+	brew install ffmpeg
---
+++ gifv.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+function printHelpAndExit {
+cat <<EOF
+Usage:
+  gifv [options] filename
+Version:   1.0.0
+
+Convert GIFs and videos into GIF-like videos
+
+Options: (all optional)
+  c CROP:     The x and y crops, from the top left of the image (e.g. 640:480)
+  o OUTPUT:   The basename of the file to be output. The default is the basename
+              of the input file.
+  r FPS:      Output at this (frame)rate.
+  s SPEED:    Output using this speed modifier. The default is 1 (equal speed).
+  O OPTIMIZE: Change the compression level used (1-9), with 1 being the fastest,
+              with less compression, and 9 being the slowest, with optimal com-
+              pression.  The default compression level is 6.
+  p SCALE:    Rescale the output (e.g. 320:240)
+  x:          Remove the original file
+
+Example:
+  gifv -c 240:80 -o my-gifv.mp4 -x my-movie.mov
+
+EOF
+exit $1
+}
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+# Initialize variables
+levels=(ultrafast superfast veryfast faster fast medium slow slower veryslow)
+level=6
+
+OPTERR=0
+
+while getopts "c:o:p:r:s:O:xh" opt; do
+  case $opt in
+    c) crop=$OPTARG;;
+    h) printHelpAndExit 0;;
+    o) output=$OPTARG;;
+    p) scale=$OPTARG;;
+    r) fps=$OPTARG;;
+    s) speed=$OPTARG;;
+    O) level=$OPTARG;;
+    x) cleanup=1;;
+    *) printHelpAndExit 1;;
+  esac
+done
+
+shift $(( OPTIND - 1 ))
+
+filename="$1"
+
+if [ -z "$output" ]; then
+  # Strip off extension and add new extension
+  ext="${filename##*.}"
+  output="$(basename "$filename" ".$ext").mp4"
+fi
+
+if [ -z "$filename" ]; then printHelpAndExit 1; fi
+
+# Video filters (scale, crop, speed)
+if [ $crop ]; then
+  crop="crop=${crop}:0:0"
+else
+  crop=
+fi
+
+if [ $scale ]; then
+  scale="scale=${scale}"
+else
+  scale=
+fi
+
+if [ $speed ]; then
+  speed="setpts=$(bc -l <<< "scale=4;1/${speed}")*PTS"
+else
+  speed=
+fi
+
+# Convert GIFs
+if [ $(file -b --mime-type "$filename") == image/gif ]; then
+  giffix="scale='if(eq(mod(iw,2),0),iw,iw-1)':'if(eq(mod(ih,2),0),ih,ih-1)'"
+else
+  giffix=
+fi
+
+# Concatenate options into a video filter string
+# giffix must be applied after any scaling/cropping
+if [ $scale ] || [ $crop ] || [ $speed ] || [ $giffix ]; then
+  filter="-vf $(join , $scale $crop $speed $giffix)"
+else
+  filter=
+fi
+
+# FPS
+if [ $fps ]; then
+  fps="-r $fps"
+else
+  fps=
+fi
+
+# Optimization level
+#   1: (fastest, worst compression)
+#   9: (slowest, best compression)
+
+(( $level > 9 )) && level=9 # OR err
+(( $level < 1 )) && level=1 # OR err
+optimize="${levels[$level]}"
+
+# TODO: Offer further optimizations
+#   Contrast frame rate : -crf 22
+#   Bit rate : -b:v 1000k
+
+# Codecs:
+#   webm:  libvpx
+#   h.264: libx264
+codec="-c:v libx264"
+
+# Verbosity
+verbosity="-loglevel panic"
+
+
+ffmpeg $verbosity -i "$filename" $codec $filter $fps -an -pix_fmt yuv420p -preset "$optimize" -movflags faststart "$output"
+
+
+if [ $cleanup ]; then
+  rm "$filename"
+fi
